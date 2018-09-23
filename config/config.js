e9e16b26f63d89f95adc217b447447af2c325c9a
nidhal-ferjani <nidhal.ferjani@gmail.com>
2018-09-23 15:21:11 +0100

    Update config to use JSON file

--- config/config.js
+++ config/config.js
@@ -2,12 +2,26 @@ var env = process.env.NODE_ENV || 'development';
 
 console.log('**** env*****',env);
 
-if(env === 'development'){
-    process.env.PORT = 3500;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-}else if(env === 'test'){
-    process.env.PORT = 3500;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppc';
-}else{
-    process.env.MONGODB_URI = 'mongodb://nidhal:azertyuiop1234@ds255282.mlab.com:55282/db-todos'
+if(env === 'development' ||  env === 'test'){
+    let config = require('./config.json');
+    let envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key)=>{
+        process.env[key] = envConfig[key];      
+    });
 }
+
+
+
+
+
+
+// if(env === 'development'){
+//     process.env.PORT = 3500;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// }else if(env === 'test'){
+//     process.env.PORT = 3500;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppc';
+// }else{
+//     process.env.MONGODB_URI = 'mongodb://nidhal:azertyuiop1234@ds255282.mlab.com:55282/db-todos'
+// }
